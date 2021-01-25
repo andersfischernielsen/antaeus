1ae2091f85a03d597cd762f2bc2fe53a83ac377c
Anders Fischer-Nielsen <andersfischern@me.com>
2021-01-25 13:18:24 +0100

    Switch to Kotlin-specific Jackson version to allow Invoice serialization
    This allows serializing Invoices to JSON when transferring these back
    after executing the billing logic.

--- pleo-antaeus-rest/build.gradle.kts
+++ pleo-antaeus-rest/build.gradle.kts
@@ -9,5 +9,5 @@ dependencies {
     implementation(project(":pleo-antaeus-models"))
 
     implementation("io.javalin:javalin:3.7.0")
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.10.3")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.12.+")
 }
